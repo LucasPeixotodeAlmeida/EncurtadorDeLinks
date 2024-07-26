c6d68ff228ffe4bbfa56d44e24cff05ce37ab56d
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-07-26 14:18:06 -0300

    Criação da entidade de link

--- src/main/java/dev/lucas/encurtador/Links/Link.java
+++ src/main/java/dev/lucas/encurtador/Links/Link.java
@@ -1,4 +1,31 @@
 package dev.lucas.encurtador.Links;
 
+import jakarta.persistence.*;
+import jdk.jfr.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "links")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Link {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String urlOriginal;
+    private String urlNova;
+    private String urlQrCode;
+
+    @Timestamp
+    private LocalDateTime createdAt;
 }
