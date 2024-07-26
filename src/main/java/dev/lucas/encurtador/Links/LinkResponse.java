21cb6b6fcc3af6e0dedf3767e385a5211081baa3
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-07-26 16:42:46 -0300

    Criação do linkResponse

--- src/main/java/dev/lucas/encurtador/Links/LinkResponse.java
+++ src/main/java/dev/lucas/encurtador/Links/LinkResponse.java
@@ -1,4 +1,21 @@
 package dev.lucas.encurtador.Links;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LinkResponse {
+
+    private UUID id;
+    private String urlOriginal;
+    private String urlNova;
+    private String urlQrCode;
 }
