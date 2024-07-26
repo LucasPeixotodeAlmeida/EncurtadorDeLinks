33d103c316c990b35b0b8b02fa89fdb2b66051d3
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-07-26 16:43:26 -0300

    Criação do linkRepository

--- src/main/java/dev/lucas/encurtador/Links/LinkRepository.java
+++ src/main/java/dev/lucas/encurtador/Links/LinkRepository.java
@@ -1,2 +1,8 @@
-package dev.lucas.encurtador.Links;public class LinkRepository {
+package dev.lucas.encurtador.Links;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LinkRepository extends JpaRepository<Link, UUID> {
 }
