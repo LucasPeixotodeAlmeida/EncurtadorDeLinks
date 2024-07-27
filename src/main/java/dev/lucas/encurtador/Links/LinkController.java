050c0abc5ea8ba69df90fa511581eaf99e78e6ff
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-07-27 20:07:06 -0300

    Criação da controller retornando a urlNova

--- src/main/java/dev/lucas/encurtador/Links/LinkController.java
+++ src/main/java/dev/lucas/encurtador/Links/LinkController.java
@@ -1,2 +1,37 @@
-package dev.lucas.encurtador.Links;public class LinkController {
+package dev.lucas.encurtador.Links;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+@RestController
+public class LinkController {
+
+    @Autowired
+    private LinkService linkService;
+
+    @PostMapping("/encurta")
+    public ResponseEntity<LinkResponse> gerarUrlEncurtada(@RequestBody Map<String, String> request) {
+        String urlOriginal = request.get("urlOriginal");
+        Link link = linkService.encurtarUrl(urlOriginal);
+
+        String gerarUrldeRedirecionamentoDoUsuario = "http://localhost:8090/r/" + link.getUrlNova();
+
+        String createdAtString = link.getCreatedAt().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+
+        LinkResponse linkResponse = new LinkResponse(
+                link.getId(),
+                link.getUrlOriginal(),
+                gerarUrldeRedirecionamentoDoUsuario,
+                link.getUrlQrCode(),
+                createdAtString
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(linkResponse);
+    }
 }
