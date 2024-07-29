5855b32514bdf8fb31ae4aafc0888c85dba9109c
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-07-29 20:54:50 -0300

    Adicionando qrcode ao linkService

--- src/main/java/dev/lucas/encurtador/Links/LinkService.java
+++ src/main/java/dev/lucas/encurtador/Links/LinkService.java
@@ -1,13 +1,21 @@
 package dev.lucas.encurtador.Links;
 
+import com.google.zxing.WriterException;
+import dev.lucas.encurtador.QRCode.QRCodeService;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Base64;
 
 @Service
 public class LinkService {
     private LinkRepository linkRepository;
+
+    @Autowired
+    private QRCodeService qrCodeService;
 
     public LinkService(LinkRepository linkRepository) {
         this.linkRepository = linkRepository;
@@ -18,12 +26,21 @@ public class LinkService {
         return RandomStringUtils.randomAlphanumeric(5, 10);
     }
 
-    public Link encurtarUrl(String urlOriginal) {
+    public Link encurtarUrl(String urlOriginal) throws WriterException, IOException {
         Link link = new Link();
         link.setUrlOriginal(urlOriginal);
         link.setUrlNova(gerarUrl());
         link.setCreatedAt(LocalDateTime.now());
-        link.setUrlQrCode("QR INDISPONIVEL");
+
+        // Gera a URL curta para redirecionamento
+        String urlCurta = "http://localhost:8090/r/" + link.getUrlNova();
+
+        // Gera a imagem do QR code e converte para base64
+        byte[] qrCodeImage = qrCodeService.generateQRCodeImage(urlCurta, 200, 200);
+        String qrCodeBase64 = Base64.getEncoder().encodeToString(qrCodeImage);
+
+        // Define a URL do QR code no formato base64
+        link.setUrlQrCode("data:image/png;base64," + qrCodeBase64);
 
         return linkRepository.save(link);
     }
