cba8c7ee296c186ddc3210c22e89dce556317905
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-07-29 20:53:22 -0300

    Criação do qrcode service

--- src/main/java/dev/lucas/encurtador/QRCode/QRCodeService.java
+++ src/main/java/dev/lucas/encurtador/QRCode/QRCodeService.java
@@ -1,2 +1,33 @@
-package dev.lucas.encurtador.QRCode;public class QRCodeService {
+package dev.lucas.encurtador.QRCode;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class QRCodeService {
+
+    // Método para gerar a imagem de QR code como um array de bytes
+    public byte[] generateQRCodeImage(String text, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        Map<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+
+        // Cria a matriz de bits que representa o QR code
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, hints);
+
+        // Converte a matriz de bits em uma imagem PNG e armazena como um array de bytes
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+        return pngOutputStream.toByteArray();
+    }
 }
